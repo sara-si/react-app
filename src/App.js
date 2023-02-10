05643fc11e4483b438cbf1e638715d71912d3cec
Sara <sara.sarabii93@gmail.com>
2023-02-10 13:09:30 +0330

    created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <body>
+        <Weather />
+        <p>
+          <a href="">Open-source code</a> by Sara Sarabi
+        </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
